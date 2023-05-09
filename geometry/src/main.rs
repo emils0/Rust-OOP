0fad347ebddb40681ab1d5350cbf030b4f989e1b
emil <emil@emil>
2023-05-09 07:26:29 +0200

    Small change.

--- geometry/src/main.rs
+++ geometry/src/main.rs
@@ -3,6 +3,7 @@ use rectangle::Rectangle;
 use right_triangle::RightTriangle;
 use shape::Shape;
 use square::Square;
+use std::f64::consts::PI;
 use trapezoid::Trapezoid;
 
 mod parallelogram;
@@ -16,7 +17,7 @@ fn main() {
     let shapes: Vec<Box<dyn Shape>> = vec![
         Box::new(Rectangle::new(4.0, 2.0, 4.0, 2.0)),
         Box::new(Square::new(3.0)),
-        Box::new(Parallelogram::new(5.0, 3.0, std::f64::consts::PI / 3.0)),
+        Box::new(Parallelogram::new(5.0, 3.0, PI / 3.0)),
         Box::new(Trapezoid::new(3.0, 5.0, 2.0, 4.0)),
         Box::new(RightTriangle::new(3.0, 4.0)),
     ];
