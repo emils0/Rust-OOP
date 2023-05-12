f9d1e63e407772b9cc21e5a76553e8dc526deb66
Emil <7209247-emil-s@users.noreply.gitlab.com>
2023-05-12 08:29:46 +0200

    Added tea.

--- coffee-machine/src/main.rs
+++ coffee-machine/src/main.rs
@@ -4,9 +4,11 @@ mod coffee_machine;
 mod filter;
 mod liquid_tank;
 mod power_button;
+mod tea_leaves;
 
 use coffee::Coffee;
 use coffee_machine::CoffeeMachine;
+use tea_leaves::TeaLeaves;
 
 use crate::{filter::Filter, liquid_tank::LiquidTank};
 
@@ -14,21 +16,37 @@ fn main() {
     let mut coffee_machine = CoffeeMachine::new(Filter::new(Some(Coffee)));
     let mut pot = LiquidTank::new(1400);
 
-    match coffee_machine.add_water(1600) {
-        Ok(_) => println!("Water added to the coffee machine."),
-        Err(e) => println!("{}", e),
-    }
+    coffee_machine.add_water(1600).unwrap();
+    println!("Added 1600 mL water to the coffee machine.");
 
     coffee_machine.power.flip_switch();
 
-    match coffee_machine.brew(&mut pot) {
-        Ok(_) => println!("Coffee brewed and poured into the pot."),
-        Err(e) => println!("{}", e),
-    }
+    coffee_machine.brew(&mut pot).unwrap();
+    println!("Coffee brewed and poured into the pot.");
 
     println!(
         "The pot now contains {} mL of {}.",
         pot.current_volume(),
         pot.current_liquid_type().unwrap_or(&"nothing".to_string())
+    );
+
+    // part 2 (tea)
+    coffee_machine.filter.add(TeaLeaves).unwrap();
+    println!("Put tea leaves into the filter.");
+
+    coffee_machine.add_water(600).unwrap();
+    println!("Added 600 mL water to the coffee machine.");
+
+    let mut teapot = LiquidTank::new(1000);
+
+    coffee_machine.brew(&mut teapot).unwrap();
+    println!("Tea brewed and poured into the pot.");
+
+    println!(
+        "The teapot now contains {} mL of {}.",
+        teapot.current_volume(),
+        teapot
+            .current_liquid_type()
+            .unwrap_or(&"nothing".to_string())
     );
 }
